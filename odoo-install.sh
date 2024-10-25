6fdb9909a53f4af121ef8fb13e84083881e52f31
xxPINDOSxx <ljmnoonan@gmail.com>
2024-10-25 00:12:14 -0400

    EOD commit

--- odoo-install.sh
+++ odoo-install.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 #########################################################
-#A script to install odoo
+# A script to install odoo
 #########################################################
 
-USER="odoo17"
+
 VERSION="17.0"
 
+USER="odoo17"
+
+HOME="/home/$USER"
+
+PORT="8069"
+
+SUPERADMIN_PASSWORD="admin"
 
 
-echo -e "\n---------- Update Server ----------"
+echo -e "\n---------- Updating Server ----------"
 sudo apt-get update
 sudo apt-get upgrade -y
 
+echo -e "\n---------- Installing git ----------"
+sudo apt install git
 
-
+echo -e "\n---------- Installing PostgreSQL ----------"
+sudo apt install postgresql postgresql-client
 
 
